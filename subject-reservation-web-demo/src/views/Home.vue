4a17aa1f14448cf6802fad9e4a73ed01c409eac5
Xhamsterse <krit.bank@mail.kmutt.ac.th>
2021-08-07 11:14:14 +0700

    update title name

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by jack
+      SIT Subject Reservation Web by jackk
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
